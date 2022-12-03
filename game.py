041705e88f28b3ae0660898edd9770eb1c24ba35
simsam8 <simvedaa@gmail.com>
2022-12-03 21:17:26 +0100

    out of bounds method

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ from player import Player
 
 FPS = 60
 PLAYER_MOVEMENT_SPEED = 300/FPS
+DIMENSION = (600,600)
 
 
 class Game:
@@ -14,12 +15,34 @@ class Game:
     
     def __init__(self) -> None:
         pygame.init()
-        self.screen = pygame.display.set_mode((600,600))
+        self.screen = pygame.display.set_mode(DIMENSION)
         #self.background = pygame.image.load('./imgs/space.jpg')
         
         self.clock = pygame.time.Clock()
         self.time_elapsed = 0
-        
+    
+    
+    def out_of_bounds(self, object):
+        oX,oY = object.centre
+        newX,newY = object.centre
+        # top
+        if oY < 0:
+            newY = 0
+            print("OUT OF BOUNDS")
+        # bottom
+        if oY > DIMENSION[1]:
+            newY = DIMENSION[1]
+            print("OUT OF BOUNDS")
+        # left
+        if oX < 0:
+            newX = 0
+            print("OUT OF BOUNDS")
+        # right
+        if oX > DIMENSION[0]:
+            newX = DIMENSION[0]
+            print("OUT OF BOUNDS")
+            
+        return [newX,newY]
         
     def display_fps(self):
         font = pygame.font.SysFont("Arial", 18, bold=True)
@@ -57,15 +80,16 @@ class Game:
                         arrow.move(PLAYER_MOVEMENT_SPEED, "H")
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_w or event.key == pygame.K_s:
-                        arrow.dY = 0
+                        arrow.dY = 0 #-= PLAYER_MOVEMENT_SPEED/2
                     if event.key == pygame.K_a or event.key == pygame.K_d:
-                        arrow.dX = 0
+                        arrow.dX = 0 #-= PLAYER_MOVEMENT_SPEED/2
             
             arrow.centre[1] += arrow.dY
             arrow.centre[0] += arrow.dX
+            arrow.centre = game.out_of_bounds(arrow)
             arrow.load_player(arrow.rect.x, arrow.rect.y)
             arrow.debug()
-            print(arrow.dX)
+            #print(arrow.dX)
             game.display_fps()
             pygame.display.update()
 
