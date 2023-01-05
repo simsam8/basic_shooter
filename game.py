ed544100d40420d43bbb5a5ccd04f5f34945fdfe
simsam8 <simvedaa@gmail.com>
2023-01-05 14:53:52 +0100

    linting

--- game.py
+++ game.py
@@ -3,73 +3,66 @@ import math
 from player import Player
 
 
-
-
 FPS = 60
-PLAYER_MOVEMENT_SPEED = 300/FPS
-DIMENSION = (600,600)
+PLAYER_MOVEMENT_SPEED = 300 / FPS
+DIMENSION = (600, 600)
 
 
 class Game:
-    
-    
     def __init__(self) -> None:
         pygame.init()
         self.screen = pygame.display.set_mode(DIMENSION)
-        #self.background = pygame.image.load('./imgs/space.jpg')
-        
+        # self.background = pygame.image.load('./imgs/space.jpg')
+
         self.clock = pygame.time.Clock()
         self.time_elapsed = 0
-    
-    
+
     def out_of_bounds(self, object):
-        '''
+        """
         Check if an object if out of bounds
-        
+
         returns position inside of screen
-        
+
         returns [newX,newY]
-        '''
-        oX,oY = object.centre
-        newX,newY = object.centre
+        """
+        oX, oY = object.centre
+        newX, newY = object.centre
         # top
         if oY < 0:
             newY = 0
-            #print("OUT OF BOUNDS")
+            # print("OUT OF BOUNDS")
         # bottom
         if oY > DIMENSION[1]:
             newY = DIMENSION[1]
-            #print("OUT OF BOUNDS")
+            # print("OUT OF BOUNDS")
         # left
         if oX < 0:
             newX = 0
-            #print("OUT OF BOUNDS")
+            # print("OUT OF BOUNDS")
         # right
         if oX > DIMENSION[0]:
             newX = DIMENSION[0]
-            #print("OUT OF BOUNDS")
-            
-        return [newX,newY]
-        
+            # print("OUT OF BOUNDS")
+
+        return [newX, newY]
+
     def display_fps(self):
         font = pygame.font.SysFont("Arial", 18, bold=True)
         fps = str(int(self.clock.get_fps()))
-        fps_display = font.render(fps, 1, (255,0,0))
-        self.screen.blit(fps_display, (0,0))
-        
+        fps_display = font.render(fps, 1, (255, 0, 0))
+        self.screen.blit(fps_display, (0, 0))
+
     def run_game(self):
         arrow = Player(self.screen)
-    
-        running = True
-        
-        while running:
-            self.screen.fill((255,255,255))
-            #screen.blit(background, (0,0))
-            
-            game.clock.tick(FPS)
-            #dt = self.time_elapsed/1000.0
-            
 
+        running = True
+
+        while running:
+            self.screen.fill((255, 255, 255))
+            # screen.blit(background, (0,0))
+
+            game.clock.tick(FPS)
+            # dt = self.time_elapsed/1000.0
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -87,22 +80,20 @@ class Game:
                         arrow.move(PLAYER_MOVEMENT_SPEED, "H")
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_w or event.key == pygame.K_s:
-                        arrow.dY -= PLAYER_MOVEMENT_SPEED/5 # temporary
+                        arrow.dY -= PLAYER_MOVEMENT_SPEED / 5  # temporary
                     if event.key == pygame.K_a or event.key == pygame.K_d:
-                        arrow.dX -= PLAYER_MOVEMENT_SPEED/5 # temporary
-            
+                        arrow.dX -= PLAYER_MOVEMENT_SPEED / 5  # temporary
+
             arrow.centre[1] += arrow.dY
             arrow.centre[0] += arrow.dX
             arrow.centre = game.out_of_bounds(arrow)
             arrow.load_player(arrow.rect.x, arrow.rect.y)
             arrow.debug()
-            #print(arrow.dX)
+            # print(arrow.dX)
             game.display_fps()
             pygame.display.update()
 
 
-    
-    
-if __name__ == '__main__':
+if __name__ == "__main__":
     game = Game()
     game.run_game()
